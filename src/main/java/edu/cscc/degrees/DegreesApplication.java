9e5dc28e460a086713cb1c8bc22d413558eb3ab9
Paul Azizeh <pazizeh@student.cscc.edu>
2021-04-27 12:18:00 -0400

    initial commit

---
+++ src/main/java/edu/cscc/degrees/DegreesApplication.java
@@ -0,0 +1,13 @@
+package edu.cscc.degrees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DegreesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DegreesApplication.class, args);
+	}
+
+}
